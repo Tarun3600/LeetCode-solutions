2dc5894bc71ea1d08b658519434498159bc43472
Tarun T <122126296+Tarun3600@users.noreply.github.com>
2025-01-19 17:08:13 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (5.42%) - LeetSync

---
+++ 1666-make-the-string-great/make-the-string-great.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string makeGood(string s) {
+        stack<int> st;
+        for(int i=0;i<s.length();i++)
+        {
+            if(!st.empty() && abs(s[i] - st.top()) == 32)
+            {
+                st.pop();
+            }
+            else
+            {
+                st.push(s[i]);
+            }
+        }
+
+        //stack reconstruction
+        string res;
+        if(st.empty())
+        return "";
+        while(!st.empty())
+        {
+            res += st.top();
+            st.pop();
+        }
+        reverse(res.begin(),res.end());
+        return res;
+    }
+};
