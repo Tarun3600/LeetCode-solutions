a5cecc6e92e7c038d46dcdf647f63c44ec57babf
Tarun T <122126296+Tarun3600@users.noreply.github.com>
2025-11-01 18:24:50 +0530

    Time: 104 ms (43.10%) | Memory: 263.1 MB (89.36%) - LeetSync

--- 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.cpp
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.cpp
@@ -1,26 +1,41 @@
-#include <vector>
-#include <algorithm>  // For binary_search
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* modifiedList(vector<int>& nums, ListNode* head) {
-        sort(nums.begin(), nums.end());  
-        ListNode* dummy = new ListNode(0);
-        dummy->next = head;
-
-        ListNode* prev = dummy;
-        ListNode* nxt = head;
-
-        while (nxt != nullptr) {
-            if (binary_search(nums.begin(), nums.end(), nxt->val)) {
-                prev->next = nxt->next; 
-                nxt = nxt->next;
-            } else {
-                prev = nxt;
-                nxt = nxt->next;
-            }
+        unordered_set<int> st;
+        for(int i=0;i<nums.size();i++)
+        {
+            st.insert(nums[i]);
         }
 
-        return dummy->next;
+        while(head != nullptr && st.count(head->val) == 1)
+        {
+            head = head->next;
+        }
+
+        ListNode* temp = head;
+        ListNode* prev = head;
+        while(temp != nullptr)
+        {
+            if(st.count(temp->val) == 1)
+            {
+                prev->next = temp->next;
+            }
+            else
+            {
+                prev = temp;
+            }
+            temp = temp -> next;
+        }
+        return head;
     }
 };
