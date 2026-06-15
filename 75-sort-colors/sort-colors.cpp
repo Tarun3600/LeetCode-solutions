7e7d383e6f6284f181ae9f3baad9a66603af8233
Tarun T <122126296+Tarun3600@users.noreply.github.com>
2026-06-15 16:54:32 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (47.93%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -3,9 +3,9 @@ public:
     void sortColors(vector<int>& nums) {
         //bubble sort
         int n = nums.size();
-        while(n >= 0)
+        while(n > 1)
         {
-            for(int i=0;i<nums.size() - 1;i++)
+            for(int i=0;i<n-1;i++)
             {
                 if(nums[i] > nums[i+1])
                 {
