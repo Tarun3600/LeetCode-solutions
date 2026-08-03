70dacd4636aba54e44b8ceda5940bbbb4f560413
Tarun T <122126296+Tarun3600@users.noreply.github.com>
2026-08-03 23:39:15 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (94.27%) - LeetSync

---
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        if(word.length() <= 8)
+        return word.length();
+        else if(word.length() > 8 && word.length() <= 16)
+        {
+            return 8 + (word.length() - 8) * 2;
+        }
+        else if(word.length() > 16 && word.length() <= 24)
+        {
+            return 8 + 16 + (word.length() - 16) * 3;
+        }
+        else if(word.length() > 24)
+        {
+            return 8 + 16 + 24 + (word.length() - 24) * 4;
+        }
+        return -1;
+    }
+};
